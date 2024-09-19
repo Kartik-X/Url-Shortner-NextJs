3d449cb6a05add727b6ac9a6d413cc2e788553bd
Mohini kulkarni <meta619012@gmail.com>
2024-09-19 19:06:50 +0530

    Made UI page for url-shortner

--- app/page.js
+++ app/page.js
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <div className="text-danger fs-5 text-center">url-shortner</div>
+      <div className="min-vh-100 d-flex flex-column align-items-center justify-content-center bg-success-subtle">
+        <div className="p-5 bg-body-secondary rounded-4 shadow-lg w-75">
+          <h1 className="fs-6  p-3 rounded-2 fw-bolder text-center text-bg-primary">
+            URL SHORTNER
+          </h1>
+
+          <form action="" className=""></form>
+          <input
+            type="text"
+            placeholder="Enter URL"
+            name="originalUrl"
+            className="form-control border border-2 border-primary"
+          />
+          <button className="btn btn-success">Shorten</button>
+        </div>
+        <div className="mt-4 text-center">
+          <Link href="/urls">
+            <span className="btn btn-danger w-100">
+              View all Shortened URLS
+            </span>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
